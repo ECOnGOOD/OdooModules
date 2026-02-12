724fff63e356eb4dd86e99a2b5a35226a26b8dd6
Gabriel <gabriel.geck@econgood.org>
2026-02-12 18:45:14 +0100

    seems to work well

--- econgood_extra_fields/__manifest__.py
+++ econgood_extra_fields/__manifest__.py
@@ -1,7 +1,7 @@
 # __manifest__.py
 {
     'name': 'ECOnGOOD Extra Fields',
-    'version': '18.0.1.0.1',
+    'version': '18.0.1.0.3',
     'category': 'ECOnGOOD',
     'summary': 'Adds extra required fields (incl. dependencies) to contacts.',
     'author': 'Gabriel Geck',
@@ -9,7 +9,8 @@
         'base', 
         'contacts',
         'partner_company_type', # Depends on OCA module
-        'partner_contact_gender'  # Depends on OCA module
+        'partner_contact_gender',  # Depends on OCA module
+        'partner_contact_birthdate',  # Depends on OCA module
     ],
     'data': [
         'views/res_partner_views.xml',
--- econgood_extra_fields/models/res_partner.py
+++ econgood_extra_fields/models/res_partner.py
@@ -1,9 +1,10 @@
+from dateutil.relativedelta import relativedelta
 from odoo import models, fields, api
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
-    # 1. Demographics (Specific to companies and/or Commune/Region partners)
+    # Demographics (specific to companies and/or Commune/Region partners)
     employee_count = fields.Integer(
         string="Number of Employees",
         help="Employee count (FTE)."
@@ -13,13 +14,13 @@ class ResPartner(models.Model):
         help="Population count if this partner represents a region or commune."
     )
 
-    # 2. Nonprofit Status
+    # Nonprofit Status
     is_nonprofit = fields.Boolean(
         string="Nonprofit Organization",
         default=False
     )
 
-    # 3. Legal/Compliance Dates
+    # Legal/Compliance Dates
     code_of_conduct_signed_date = fields.Date(
         string="Code of Conduct Signed On"
     )
@@ -27,7 +28,7 @@ class ResPartner(models.Model):
         string="Privacy Agreement Signed On"
     )
 
-    # 4. Special Email
+    # Special Email
     # Note: Use Char, but standard Odoo handles email validation on 'email' field. -> this will not be needed in the future, when contacts are linked to users (TODO)
     email_econgood = fields.Char(
         string="ECOnGOOD Email Address"
